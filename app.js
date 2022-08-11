8f0d15d1b3c50a1ead5124778bc48526e65035ee
Ashik19aug <ashik19aug@gmail.com>
2022-08-11 20:27:39 +0600

    install express

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const http = require("http");
+const express = require('express');
 
-const server = http.createServer(((req, res) => {
-    if(req.url === '/'){
-        res.write('play with nodejs Home page');
-        res.end();
-    }
-    if(req.url === '/login'){
-        res.write('play with nodejs Login page');
-        res.end();
-    }
-    else {
-        res.writeHead(200, {'Content-Type': "application/json"});
-        res.end(JSON.stringify({data: 'page not found'}));
-    }
+const app = express();
 
-}))
+const port = 2222;
 
-console.log(`Project is running at http://localhost:${2222}`)
-server.listen(2222);
+app.get('/', (req, res) => {
+    res.send('Hello node app!');
+})
+
+app.listen(port,()=> {
+    console.log(`Server running at http://127.0.0.1: + ${port}`);
+})
+
+// const http = require("http");
+//
+// const server = http.createServer(((req, res) => {
+//     if(req.url === '/'){
+//         res.write('<h1>Home page</h1>');
+//         res.end();
+//     }
+//     if(req.url === '/login'){
+//         res.write('<h1>login Page</h1>');
+//         res.end();
+//     }
+//     else {
+//         res.writeHead(200, {'Content-Type': "text/html"});
+//         res.write('<h1>page not found</h1>');
+//         res.end();
+//     }
+// }))
+
+// console.log(`Project is running at http://localhost:${2222}`)
+// server.listen(2222);
