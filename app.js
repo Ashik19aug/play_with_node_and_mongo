4d96d230233705fd9a9b06c923f44c87d6abf8de
Ashik19aug <ashik19aug@gmail.com>
2022-08-11 15:38:56 +0600

    define some url

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require("http");
 
 const server = http.createServer(((req, res) => {
-    res.write('play with nodejs');
-    res.end();
+    if(req.url === '/'){
+        res.write('play with nodejs Home page');
+        res.end();
+    }
+    if(req.url === '/login'){
+        res.write('play with nodejs Login page');
+        res.end();
+    }
+    else {
+        res.writeHead(200, {'Content-Type': "application/json"});
+        res.end(JSON.stringify({data: 'page not found'}));
+    }
+
 }))
 
 console.log(`Project is running at http://localhost:${2222}`)
