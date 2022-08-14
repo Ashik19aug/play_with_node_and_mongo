f54d8bc602c403ec59ccfcb7e534938fa1e3cdc4
Ashik19aug <ashik19aug@gmail.com>
2022-08-14 13:25:20 +0600

    croud is done

--- Routes/post/post.js
+++ Routes/post/post.js
@@ -9,13 +9,12 @@ postRouter
     .get( async (req, res) => {
         const conn = await connect();
         const posts = await conn.collection("post").find().toArray();
-        res.json( posts);
-        // res.send("get route for posts");
+        res.status(200).json( posts);
     })
     .post( async (req, res) => {
         const conn = await connect();
         await conn.collection('post').insertOne(req.body);
-        res.json({data: "A new post has been created."});
+        res.status(201).json({data: "A new post has been created."});
     });
 
 // dynamic routes
@@ -25,13 +24,20 @@ postRouter
         const _id = ObjectID(req.params.id);
         const conn = await connect();
         const post = await conn.collection('post').findOne({ _id });
-        res.json(post);
+        res.status(200).json(post);
     })
-    .patch((req, res) => {
-    res.json({data: `update route for post no : ${req.params.id}`});
+    .patch(async (req, res) => {
+        const _id = ObjectID(req.params.id);
+        const conn = await connect();
+        const updatePost = await conn.collection('post').updateOne({ _id }, {$set: req.body});
+        res.json({data: "Your Post has been updated successfully."});
+        // res.json({data: `update route for post no : ${req.params.id}`});
     })
-    .delete((req, res) => {
-    res.json({data: `delete route for post no : ${req.params.id}`});
+    .delete(async (req, res) => {
+        const _id = ObjectID(req.params.id);
+        const conn = await connect();
+        await conn.collection('post').deleteOne({ _id });
+        res.status(204).json({data: `The delete post id is : ${req.params.id}`});
     })
 
 
